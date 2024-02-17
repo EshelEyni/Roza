ebec194dbc489afa5c1df65c62f1a3bdc59ade78
Eshel Eyni <esheleyni@gmail.com>
2024-02-17 14:51:34 +0200

    first commit

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>homepage</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+<style lang="scss">
+</style>
